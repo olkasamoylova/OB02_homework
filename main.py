f6ce8fc52eb81b688780e3770df8a06dd9908ef1
olkasamoylova <olga.n.samoylova@gmail.com>
2024-09-04 17:16:02 +0200

    add class Admin

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ class User:
     def __init__(self, user_id, name):
         self.__user_id = user_id        # Приватный атрибут для ID
         self.__name = name              # Приватный атрибут для имени
-        self.__access_level = 'user'    # Приватный атрибут для уровня доступа
+        self.__access_level = 'user'    # Приватный атрибут для уровня доступа, в основном классе задает обычный уровень доступа (как всем)
 
     # Методы для получения данных пользователя, так как по умолчанию они приватные и напрямую извне не заберешь инфо
     def get_user_id(self):
@@ -13,3 +13,29 @@ class User:
 
     def get_access_level(self):
         return self.__access_level
+
+class Admin(User):
+    def __init__(self, user_id, name):
+        super().__init__(user_id, name)
+        self.__admin_access_level = 'admin'  # Приватный атрибут для особого уровня доступа администратора
+
+    # Метод для добавления пользователя
+    def add_user(self, user_list, user):
+        if self.get_access_level() == 'admin':
+            user_list.append(user)
+            print(f"Пользователь {user.get_name()} добавлен в систему.")
+        else:
+            print("У вас, коллега, недостаточно прав для добавления пользователя.")
+
+    # Метод для удаления пользователя
+    def remove_user(self, user_list, user):
+        if self.get_access_level() == 'admin':
+            if user in user_list:
+                user_list.remove(user)
+                print(f"Пользователь {user.get_name()} удален из системы.")
+            else:
+                print("Пользователь не найден в системе.")
+        else:
+            print("У вас, коллега, недостаточно прав для добавления пользователя.")
+
+
